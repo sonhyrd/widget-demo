fcefc4f172b584a2b8d44b59e4a1f6e803b383f0
sonhyrd <son.hong.do@hyrd.ai>
2025-06-11 00:41:53 +0700

    prod

--- hr-token.post.ts
+++ hr-token.post.ts
@@ -20,7 +20,7 @@ export default defineEventHandler(async (event) => {
   const expiresIn = 60 * 60 * 24 * 30 * 2 // 2 months
 
   const claims = {
-    iss: 'https://integration.app.paulsjob.ai',
+    iss: 'https://app.paulsjob.ai',
     aud: 'paul-job',
     sub: userId,
     email,
@@ -33,31 +33,31 @@ export default defineEventHandler(async (event) => {
   }
 
   const privateKeyRaw = `-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEArYGydUoG+qDLb2gD7NpPJA9HXjtrmeh/rz2CVnh8eBarolh0
-OFFdt2eX8FsI1aVJuRFuSX6WlEeS/yEH9M3O6auMrM3XfruvupCDZ/73s8G8Zl3x
-NLuYNqzoJEGSRj5jbvRYluiMT1iS5LX7mC+pPnt7OUA+sZ4HG2qykaj2bzVqOUe0
-s36GP/IGhiideH0xzM6MxB/TD0Wu1r/t4HvDEB1AhTA8iROkxWVL2BQu/GuVrGkZ
-B38OgoCQX1Q/Jz2T/06UuYHNrOmqpDt9Q4wCJf7vYL7XGYpXmJeaYDX2U+LBNT7x
-wizVdDi4MrfRt8j5YWC5rN852AgCS7P/d09I0QIDAQABAoIBADpr6F4L3gu35qgN
-evzeiTVYG6zEDzgUjCHCcUeSbD63S3VvmTp5Uzf1HrjkvjXJ4Of60YrXn/Yvn3Re
-d+JbBieyBr0M1lmq8sG79TMoK9mQC1LgoN4jqF4InGXQ2mLikPKEPewaQ1xYM6Xl
-DOEycydBxr0BP9ronfYCWDwpi0joQmDEUZXQ0gWTrU7v9OiOYfgOakXTJ3qm9VqB
-A/wgM1aCJndtjHEdXZ1fKXoqX9ME5x6qNCbgorLW83XogxY4eN24fFrihe2LDZw6
-E91dQBib6IH6w5iqOWgghHi3sgV6qzE8b23ET3P7zQhBqRfrGFRfvq2gWgEJ1FnQ
-x279DV0CgYEAypD71LxQmmRDTYfyJvMwbzueJra1t++ykSkTdkdrKIttvlBaNDPN
-2zatiEdg1JAjkUm+eP/Y9xOiJ+cGASSTgNOPoIfTKzgxlv6+8nCK0BqqQz/ZRhYi
-outJpu1BAazr8eF1geiFHYBxv4OSJqWJ1sDl4ke4H9O9qsYMOlSfZ9sCgYEA20Za
-NawjohIqGdfJfBJXghJolCEA8GYjgJhVyknW2SfZM/lSlS9lwFZ0lYdONDRhDPC9
-mE16Vlz48qDXB7KNgCMcVGijPprl9QE+IK5m7mN/oTCl1dZWUKMeopBdEP3TGw9Y
-V6LynL6SayahNxbKwy4C96FNwrPulbUu61Ezl8MCgYBmczGLBLtXesI3Yjxsyq6C
-c4qnVd1ObCLKHBZFyhU+alZU3NricBj7eAoIVOF6YHQ8tSlqlSusuvdWaxe0WEVp
-8JZBwLBiuYcLMUht4dGzMulOHerL8cIO2CVmEbTBHi4vJbKCw5EfvKjLDcZLWYn/
-XYxnB5LuNe9FgekJT2dX5QKBgQCHb3HCmvs7XJrScX0KfE2+kl4zuBrbYUrmX7EF
-jAv+/uTifEmsyaTAYuYJgZQlHIEhPATu8FPyyeQeAIgwQfGyRn12CVdjEMueQ9E6
-4F3FOoGXbvPPG18e2VHPuPkyyIQ+v4CiSIwvkmnfcvG6lAJTAbVlfCUKcJaZs1oy
-kGocfwKBgB/SBed7NSYZ/gNFV1mZmTyjH/THlXYZ2wJRpU69e19Fy2dFdx4jIjFP
-AzUeHNWoqX3tB33srIJdlobTaXBXyyFyojs4G2YclNfWpBbXQFoMrikCpRcjpOOO
-0D9txZdSgfYmXyWMiXHVTigbBuliK70BLrZoaDfUX/eJupOPMHgN
+MIIEpAIBAAKCAQEAxHyRfQMNrcD6VXblrLpR1e5UnQ52G2G3ZxdzTiDP/T5p+7vf
+v12S6iBkPeQ8NMfTHmSbH+dB05jn/sWC6xknpv75aH8f/HVdxb9TT376WTv7Q4jH
+eeeULYxYcvvwkVaenh5p4U9jXux1FzBqcL1J+wX08UnD9pdbW88jtEGNR3eIdAgf
+wCy+hRvyYtyMEtYlflqKzSlkWNjxDetnV/VDs6gEVavwi+J9TfkglgnSzlmEiXrH
+XTwiC2NAb6TxOkvn5gobXELirSm8pOLXgkB5Y6exII/wQW2K9acqFufNFV3TE6RJ
+mJDOyeFRRqOeDbIp2HDMjFuMm/eHvaO5bT0tlwIDAQABAoIBAGz3TFQQ8nAO2oW1
+F3BwL9w9fS1QjM1opqaEicylQ9OE3o/dxBtDkKtI5W6xeXYn73wmfST3QjlPzjEr
+ZgxteeER/E0oWxvOFwAIt/IZUEtWiWUNoNqJRFLyR0cVU3e2mubDpSJvvYMyDUmb
+7xuwzitpRUrfBs9lZyQGPx3DUM91kNehe939fHGSkrRiSkZZSf6MJjxSGp9Nihg5
+yo5ydMrrmzDIw9lX8wmq6MWUHJKFpCPD4It4HyO/HHNX2Z/OvOss2r1jjhvA+qqO
+ZAcFsRXE+pbGNB1nsRRQHqgE50PvVLtgieMumi5UEjnQppmt14mm/2tz77OXLjVE
+hyPj4PECgYEA1I5BmaWtGgtqwWGj0dFWi2fTAXW7utN1jv//k6OE4mEoJXzIjs5p
+LYqDauKQgjahPIzfHa+ZwnDTr0Ss29Ce271VLVpaYbtPjVPcDbA9QofvC5XjZe93
+FrboIj1q8hmjRnwoFHkUl0VXyNHKhdulNcgGkK+3F1T/VOLmNerRj98CgYEA7KWD
+q1/9BdFQheFtQRDoW+3moHLDITegKNc+EdeF6vdA9pamgadqKkXoZ6IgVPK8YgnH
+jKKQZqJbN7Jb9la7r6CyPVoc/we8dTTb1RK5Y7OiuXLcaqBuDRkzzpAae3rrAvYv
+QuGRhW9Dhc8QPji9KW4ZJ4J9HlPC9RmXn43duUkCgYEAhcrVeAVVJGOzWu4+079V
+HoLHys90z3BzOwPgt+nhpy1Iu/ADvgAnLEdX2VurYotIiniRqHnz6vRiCpzLwcFb
+fS18BAQvh/0DYg47IVKh/NLboQtEC9HjwCR5kbPfMWz53VAhWmGsmFtJmlqThBZx
+s5yopUI1mTYjUidQiPe52TkCgYAgv0K6O107Kdz3udsxjtJorkIk39yqwxTRsf9v
+3qA0hKSjuvMtq+ogEW54sNHCj5iMBIhtqK1M1pjC21aofxYX1qfzn321uO9WVMs6
+8A1hg6E4AnIH+01fWKZ3pi9T+Q1+amzd13MYwO3aHW5E/fsHOaoXcpI32SV4X2NA
+/BO7qQKBgQCnk6JgN/0peoI3z/FSxrsql/xa/hKtsqOUhALpcZfe4LBME8+jbxKT
+6qaeq069AaU0O2yxE5+18QW/TfwV7C00EihrjlZtVUPf0+4CpgxEN4cl2pIwpdV9
+YNGNRchNQEQXvWG2OKHjmsebeSE7UsP9QUbiGlRO0+sKYUoidnQjkw==
 -----END RSA PRIVATE KEY-----`
 
   const runtimeConfig = useRuntimeConfig()
@@ -76,7 +76,7 @@ AzUeHNWoqX3tB33srIJdlobTaXBXyyFyojs4G2YclNfWpBbXQFoMrikCpRcjpOOO
   const signOptions: jwt.SignOptions = {
     algorithm: 'RS256',
     // keyid: runtimeConfig.ssoKeyId,
-    keyid: '20250528-f216',
+    keyid: '20250610-86d0',
   }
 
   let token
